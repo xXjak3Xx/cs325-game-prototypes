e19a758e68fa1404dd7a4777978b841275738f66
Jake <jcrawf20@gmu.edu>
2019-02-14 03:26:51 -0500

    some stuff works

--- game2/js/main.js
+++ game2/js/main.js
@@ -38,20 +38,27 @@ var config = {
 		level = map.createStaticLayer('Tile Layer 1', tileset, 0, 0);
 		level.setCollisionByProperty({collision: true});
 		
-		player1 = this.physics.add.sprite(500, 470, 'soldier');
+		gold_gen = 2;
+		
+		player1 = this.physics.add.sprite(100, 470, 'soldier');
 		this.physics.add.collider(player1, level);				
 		this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
 		player1.setCollideWorldBounds(true);
-		player1.setData({name: "player 1", hp: 100, attack: 25, gold: 50});
+		player1.setData({name: "player 1", hp: 100, attack: 25, gold: 50, kills: 0, ready: true});
 		player1.setDisplaySize(50, 50); 
 		
 		
-		player2 = this.physics.add.sprite(500, 470, 'evil_soldier');
+		player2 = this.physics.add.sprite(700, 470, 'evil_soldier');
 		this.physics.add.collider(player2, level);
 				
 		player2.setCollideWorldBounds(true);
-		player2.setData({name: "player 2", hp: 100, attack: 25, gold: 50});
+		player2.setData({name: "player 2", hp: 100, attack: 25, gold: 50, kills: 0});
 		player2.setDisplaySize(50, 50);
+		
+		
+		good_gold = this.add.text(100, 100, player1.data.values.gold + "g \nkills: " + player1.data.values.kills + "\nHP: " + player1.data.values.hp);
+		evil_gold = this.add.text(700, 100, player2.data.values.gold + "g \nkills: " + player2.data.values.kills + "\nHP: " + player2.data.values.hp);
+		game_over = this.add.text(200, 240, '', { fontSize: '32px', fill: '#fff' });
 		
 		
 		enemyGroup = this.physics.add.group();
@@ -69,6 +76,13 @@ var config = {
 		
 		this.physics.add.collider(player1, enemyGroup);
 		this.physics.add.collider(player2, friendGroup);
+		
+		
+		timer = this.time.addEvent({
+			delay: 1000,
+			callback: passive_gold,
+			loop:true
+		});
 		
 		
 		this.anims.create({
@@ -161,6 +175,17 @@ var config = {
     		repeat: -1
 		});
     }
+	
+	function passive_gold(){
+		player1.data.values.gold += gold_gen;
+		player2.data.values.gold += gold_gen;
+		update_text();
+	}
+	
+	function update_text(){
+		good_gold.setText(player1.data.values.gold + "g \nkills: " + player1.data.values.kills + "\nHP: " + player1.data.values.hp);
+		evil_gold.setText(player2.data.values.gold + "g \nkills: " + player2.data.values.kills + "\nHP: " + player2.data.values.hp);
+	}
 
 // hp, att, spd	
 var goodBlobs = [[50, 20, 40], //soldier
@@ -176,7 +201,104 @@ var badBlobs = [[50, 20, 40], //soldier
         evil_cursors = this.input.keyboard.createCursorKeys();
 		cursors = this.input.keyboard.addKeys({left:"A",right:"D",up:"W",down:"S",sold:"ONE",ranged:"TWO",theif:"THREE",evil_sold:"OPEN_BRACKET",evil_ranged:"CLOSED_BRACKET",evil_theif:"BACK_SLASH"});
 		input();
+		fight();
     }
+	
+	
+	function fight(){
+		let good_kids = friendGroup.getChildren();
+		let bad_kids = enemyGroup.getChildren();
+		console.log(friendGroup.getFirst());
+		
+		for(let i = 0; i < good_kids.length; i++){
+			if(good_kids[i].data.values.name === "soldier" || good_kids[i].data.values.name === "theif"){
+				if(Math.abs(player2.body.x - good_kids[i].body.x) <= 45)
+					attack(good_kids[i], player2);
+				else if(enemyGroup.getFirst() === null)
+					return;
+				else if(Math.abs(enemyGroup.getFirst().body.x - good_kids.body.x) <= 45)
+					attack(good_kids[i], enemyGroup.getFirst());
+			}
+			else if(good_kids[i].data.values.name === "ranged"){
+				if(Math.abs(player2.body.x - good_kids[i].body.x) <= 90)
+					attack(good_kids[i], player2);
+				else if(enemyGroup.getFirst() === null)
+					return;
+				else if(Math.abs(enemyGroup.getFirst().body.x - good_kids.body.x) <= 90)
+					attack(good_kids[i], enemyGroup.getFirst());
+			}
+		}
+		
+		
+		for(let i = 0; i < bad_kids.length; i++){
+			if(bad_kids[i].data.values.name === "evil_soldier" || bad_kids[i].data.values.name === "evil_theif"){
+				if(Math.abs(player1.body.x - bad_kids[i].body.x) <= 45)
+					attack(bad_kids[i], player1);
+				else if(friendGroup.getFirst() === null){
+					console.log('in there');
+					return;
+				}
+				else if(Math.abs(friendGroup.getFirst().body.x - bad_kids.body.x) <= 45)
+					attack(bad_kids[i], friendGroup.getFirst());
+			}
+			else if(bad_kids[i].data.values.name === "ranged"){
+				if(Math.abs(player1.body.x - bad_kids[i].body.x) <= 90)
+					attack(bad_kids[i], player1);
+				else if(friendGroup.getFirst() === null)
+					return;
+				else if(Math.abs(friendGroup.getFirst().body.x - bad_kids.body.x) <= 90)
+					attack(bad_kids[i], friendGroup.getFirst());
+			}
+		}
+	}
+	
+	function attack(attacker, attackee){
+		console.log('fight');
+		attackee.data.values.hp -= attacker.data.values.attack;
+		if(attackee.data.values.hp <= 0){
+			if(attackee === player1 || attackee === player2){
+				update_text();
+				game_over.setText("Game over. " + attackee.data.values.name + " loses.");
+				attackee.destroy();
+				friendGroup.destroy(true);
+				enemyGroup.destroy(true);
+			}
+			else{
+				friendGroup.remove(attackee, true, true);
+				enemyGroup.remove(attackee, true, true);
+			}
+		}
+		else{
+			if(attackee === player1 || attackee === player2)
+				update_text();
+		}
+	}
+	
+	
+	function buyBlob(isGood, type){
+		let price;
+		if(type == 0)	
+			price = 10;
+		else if(type == 1)
+			price = 15;
+		else
+			price = 20;
+		
+		if(isGood){
+			if(player1.data.values.gold < price)
+				return;
+			
+			player1.data.values.gold -= price;
+		}
+		else{
+			if(player2.data.values.gold < price)
+				return;
+			
+			player2.data.values.gold -= price;
+		}
+		update_text();
+		spawnBlob(isGood, type);
+	}
 	
 	//Type: [0:soldier, 1:ranged, 2:theif]
 	function spawnBlob(isGood, type){
@@ -230,20 +352,19 @@ var badBlobs = [[50, 20, 40], //soldier
 		else
 			name = "theif";*/
 		
-		minion.setData({name: name, hp: faction[type][0], attack: faction[type][1]});
-		console.log(groupType.getChildren());
+		minion.setData({name: name, hp: faction[type][0], attack: faction[type][1], ready: true});
 	}
 	
 var reset = true;
 var reset2 = true;
 	function input() {
 		if(cursors.left.isDown){
-			player1.setVelocityX(-40);
+			player1.setVelocityX(-80);
 			player1.anims.play('left-sold', true);
 			player1.flipX = false;
 		}
 		else if(cursors.right.isDown){
-			player1.setVelocityX(40);
+			player1.setVelocityX(80);
 			player1.anims.play('left-sold', true);
 			player1.flipX = true;
 		}
@@ -253,12 +374,12 @@ var reset2 = true;
 		}
 		
 		if(evil_cursors.left.isDown){
-			player2.setVelocityX(-40);
+			player2.setVelocityX(-80);
 			player2.anims.play('evil-left-sold', true);
 			player2.flipX = false;
 		}
 		else if(evil_cursors.right.isDown){
-			player2.setVelocityX(40);
+			player2.setVelocityX(80);
 			player2.anims.play('evil-left-sold', true);
 			player2.flipX = true;
 		}
@@ -270,15 +391,15 @@ var reset2 = true;
 		
 		
 		if(cursors.sold.isDown && reset){
-			spawnBlob(true, 0);
+			buyBlob(true, 0);
 			reset = false;
 		}
 		else if(cursors.ranged.isDown && reset){
-			spawnBlob(true, 1);
+			buyBlob(true, 1);
 			reset = false;
 		}
 		else if(cursors.theif.isDown && reset){
-			spawnBlob(true, 2);
+			buyBlob(true, 2);
 			reset = false;
 		}
 		else{
@@ -287,15 +408,15 @@ var reset2 = true;
 		}
 		
 		if(cursors.evil_sold.isDown && reset2){
-			spawnBlob(false, 0);
+			buyBlob(false, 0);
 			reset2 = false;
 		}
 		else if(cursors.evil_ranged.isDown && reset2){
-			spawnBlob(false, 1);
+			buyBlob(false, 1);
 			reset2 = false;
 		}
 		else if(cursors.evil_theif.isDown && reset2){
-			spawnBlob(false, 2);
+			buyBlob(false, 2);
 			reset2 = false;
 		}
 		else{
