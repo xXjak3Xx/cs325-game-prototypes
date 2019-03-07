6c5d696142c7c9b36ca8dd682b6e35e48a05597e
Jake <jcrawf20@gmu.edu>
2019-03-07 09:56:06 -0500

    got a lot of stuff working

--- game5/js/main.js
+++ game5/js/main.js
@@ -32,12 +32,59 @@ var config = {
 		player.setDisplaySize(100, 200);
 		textbox = this.add.image(400, 525, 'textbox');
 		textbox.setDisplaySize(850, 150);
-		text = this.add.text(textbox.getCenter().x, textbox.getCenter().y, "I Am the Video Game Police", {fontSize: 25}).setOrigin(.5);
+		text = this.add.text(textbox.getCenter().x, textbox.getCenter().y, "I Am the Video Game Police \n ", {fontSize: 25}).setOrigin(.5);
+		menu = this.add.text(textbox.getCenter().x, textbox.getCenter().y, "", {fontSize: 25, color:"RED"}).setOrigin(.5);
     }
+	
+	var choices = ["Roomate", "RA", "Neighbor", "Accuse"];
+	var choice = 0;
+	
+	function menuChoice(up, down){
+		if(up)
+			choice ++;
+		if(down)
+			choice --;
+		
+		if(choice >= choices.length)
+			choice = 0;
+		if(choice < 0)
+			choice = choices.length - 1;
+		
+		let temp;
+		let tempText = "";
+		let tempMenu = "";
+		for(let i = 0; i < choices.length; i++){
+			temp = choices[i];
+			
+			if(choice == i){
+				tempMenu += temp;
+				for(let j = 0; j < temp.length; j++)
+					tempText += " ";
+			}
+			else{
+				tempText += temp;
+				for(let j = 0; j < temp.length; j++)
+					tempMenu += " ";
+			}
+			
+			if(i % 2 == 0){
+				tempText += "    ";
+				tempMenu += "    ";
+			}
+			else{
+				tempText += "\n";
+				tempMenu += "\n";
+			}
+		}
+		
+		text.setText(tempText);
+		menu.setText(tempMenu);
+	}
 	
 	function start(){
 		return "You're called to Gregory Manson Unniversity to investigate a crime scene in the freshman dorm building. A student named Robert Cook was found murdered in his " +
-			"dorm room late in the night, and you've been tasked with investigating"
+			"dorm room late in the night, and you've been tasked with investigating and solving this horrible crime. The victim appears to have been killed by a single gunshot wound " +
+			"to the chest. Additionally you find the victim's phone, but unfortunately it is passcode protected so no information can be gleaned from it. Who do you wish to investigate?";
 	}
 	
 	//value is array of words to output
@@ -58,7 +105,7 @@ var config = {
 	}
     
     function update() {
-		cursors = this.input.keyboard.addKeys({select: "ENTER"});
+		cursors = this.input.keyboard.addKeys({select: "ENTER", up: "LEFT", down: "RIGHT"});
 		input();
     }
 	
@@ -81,6 +128,25 @@ var config = {
 						reading = false;
 						str = true;
 					}
+				}
+				else{
+					menuChoice(false, false);
+				}
+			}
+		}
+		else if(cursors.up.isDown){
+			if(release){
+				release = false;
+				if(!reading){
+					menuChoice(false, true);
+				}
+			}
+		}
+		else if(cursors.down.isDown){
+			if(release){
+				release = false;
+				if(!reading){
+					menuChoice(true, false);
 				}
 			}
 		}
